6c4e69d62126e54e9fe4995e26468f14a693319b
sharankalshetti161-code <sharankalshetti161@gmail.com>
2026-09-23 18:45:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "50mb" }));
 
 app.get("/", (req, res) => {
   res.send("SK Chat AI Server is running!");
